fce61f3d311fd14b61ddf11db0ad41fa70de8443
Daniel Mane <danmane@gmail.com>
2013-11-26 01:55:15 -0600

    Working C implementation

---
+++ primes.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int isPrime(int n, int primes[]){
+    int p;
+    for (int i=0; ; i++){
+        p = primes[i];
+        if (n % p == 0){
+            return 0;
+        }
+        if (p * p > n) {
+            return 1;
+        }
+    }
+}
+
+int * primes(int num){
+    int *primes = malloc(sizeof(int) * num);
+    primes[0] = 2; primes[1] = 3;
+    int found = 2;
+    int i = 2;
+    int next;
+    while (found < num) {
+        next = 6 * (i/2) + (i%2) * 2 - 1;
+        if (isPrime(next, primes)){
+            primes[found] = next;
+            found++;
+        }
+        i++;
+    }
+    return primes;
+}
+
+long primesum(int num){
+    int *list_of_primes;
+    list_of_primes = primes(num);
+    int i;
+    long sum = 0;
+    for (i=0; i<num; i++){
+        sum+=list_of_primes[i];
+    }
+    return sum;
+}
+
+int main(int argc, char *argv[]){
+    int num;
+    num = atoi(argv[1]) * 1000;
+    printf("computing %d primes\n", num);
+    if (num == 0){ num = 10000;}
+    long x = primesum(num);
+    printf("%ld\n", x);
+}
